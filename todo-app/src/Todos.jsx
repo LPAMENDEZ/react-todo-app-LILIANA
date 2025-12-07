e814cb2b5a015eba856ceeb1377831b8470de35d
LPAMENDEZ <panessoliliana785@gmail.com>
2025-12-07 07:40:08 -0500

    feat: componente Todos.jsx con fetch inicial usando useEffect

--- todo-app/src/Todos.jsx
+++ todo-app/src/Todos.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Todos() {
-  return <h1>Página Todos</h1>;
+  const API = "https://jsonplaceholder.typicode.com/users";
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get(API).then((res) => setUsers(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Usuarios</h2>
+    </div>
+  );
 }
+
 
