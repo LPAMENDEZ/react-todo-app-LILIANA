ef912295f0e5b0685a7e731c8c82bd8a9d23133c
LPAMENDEZ <panessoliliana785@gmail.com>
2025-12-07 07:45:39 -0500

    feat: implementación de listado de todos desde API

--- todo-app/src/Todos.jsx
+++ todo-app/src/Todos.jsx
@@ -3,17 +3,32 @@ import axios from "axios";
 
 export default function Todos() {
   const API = "https://jsonplaceholder.typicode.com/users";
+
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios.get(API).then((res) => setUsers(res.data));
+    const fetchData = async () => {
+      const res = await axios.get(API);
+      setUsers(res.data);
+    };
+
+    fetchData();
   }, []);
 
   return (
     <div>
-      <h2>Usuarios</h2>
+      <h2>Usuarios registrados (API)</h2>
+
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>
+            <strong>{u.name}</strong> — {u.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
 
 
